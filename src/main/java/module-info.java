57b6c9cc5656c28822f269aae67a44952857bf53
SP.Win <supawit.promma6256@gmail.com>
2024-10-15 16:17:41 +0700

    Add log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module se233.asteroids {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.apache.logging.log4j;
 
     opens se233.asteroids to javafx.fxml;
     exports se233.asteroids;
