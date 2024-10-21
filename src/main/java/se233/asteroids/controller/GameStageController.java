57716ed5f1463cca6e9a329880d966045838e5eb
RinZ <masterwinzaza@gmail.com>
2024-10-21 23:55:14 +0700

    Change how createAnimatedSprite work to accomodate other sprite

--- src/main/java/se233/asteroids/controller/GameStageController.java
+++ src/main/java/se233/asteroids/controller/GameStageController.java
@@ -21,8 +21,8 @@ public class GameStageController {
         this.playerShip.setRotate(-90);
         this.playerShipController = new PlayerShipController(playerShip);
 
-        idleSprite = createAnimatedSprite("/se233/asteroids/assets/playerShip/Idle.png", 1, 1);
-        boostSprite = createAnimatedSprite("/se233/asteroids/assets/playerShip/Boost.png", 5, 5);
+        idleSprite = createAnimatedSprite("/se233/asteroids/assets/playerShip/Idle.png", 1, 1, 192, 192);
+        boostSprite = createAnimatedSprite("/se233/asteroids/assets/playerShip/Boost.png", 5, 5, 192, 192);
 
         showAnimation("idle");
     }
@@ -33,9 +33,9 @@ public class GameStageController {
         updateAnimationVisibility();
     }
 
-    private AnimatedSprite createAnimatedSprite(String imagePath, int columns, int rows) {
+    private AnimatedSprite createAnimatedSprite(String imagePath, int columns, int rows, int width, int height) {
         Image spriteSheet = new Image(Launcher.class.getResourceAsStream(imagePath));
-        AnimatedSprite sprite = new AnimatedSprite(spriteSheet, columns, rows, 1, 0, 0, 192, 192);
+        AnimatedSprite sprite = new AnimatedSprite(spriteSheet, columns, rows, 1, 0, 0, width, height);
         sprite.setFitWidth(100);
         sprite.setFitHeight(100);
         return sprite;
