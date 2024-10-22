542c114050412a210327a93bc30933aa15f9082c
RinZ <masterwinzaza@gmail.com>
2024-10-22 22:13:49 +0700

    Create NormalAttackController

--- src/main/java/se233/asteroids/controller/NormalAttackController.java
+++ src/main/java/se233/asteroids/controller/NormalAttackController.java
@@ -1,2 +1,24 @@
-package se233.asteroids.controller;public class NormalAttackController {
+package se233.asteroids.controller;
+
+import se233.asteroids.model.NormalAttack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NormalAttackController {
+    private NormalAttack normalAttack;
+    private List<NormalAttack> normalAttackList;
+
+    public NormalAttackController(NormalAttack normalAttack) {
+        this.normalAttack = normalAttack;
+        this.normalAttackList = new ArrayList<NormalAttack>();
+    }
+
+    public List<NormalAttack> getNormalAttackList() {
+        return this.normalAttackList;
+    }
+
+    public void update() {
+
+    }
 }
